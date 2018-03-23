981263ac0d89a4025cc419aef6dd730cfb88ea33
Aleksa Jovanovic <aleksajovanovic18@gmail.com>
2018-03-23 09:15:05 -0400

    fixing sync

--- test/STKToken.js
+++ test/STKToken.js
@@ -2,78 +2,52 @@ var STKToken = artifacts.require("./STKToken.sol");
 const assertRevert = require('./helpers/assertRevert');
 const expectThrow = require('./helpers/expectThrow');
 
-const evmThrewError = (err) => {
-  if (err.toString().includes('VM Exception')) {
-    return true
-  }
-  return false
-}
-
 contract('STKToken', accounts => {
   userAccount = accounts[0];
   recipientAccount = accounts[1];
 
-  beforeEach('deploy contract for each test', async()=> {
+  beforeEach(async()=> {
       instance = await STKToken.deployed();
   });
 
-  describe('initial supply', ()=> {
+  describe('Initial supply', ()=> {
     it("Should have 1 billion tokens in first account", async()=> {
-      const balance = await instance.balanceOf(userAccount);
-      assert.equal(balance.valueOf(), 1000000000, '1 billion was not in the first account');
+        const balance = await instance.balanceOf(userAccount);
+        assert.equal(balance.valueOf(), 1000000000000000000, '1 billion was not in the first account');
     });
   });
 
-  describe('vanity check', ()=> {
-    it('Should have symbol as STK', async()=> {
+  describe('Vanity check', ()=> {
+    it('Should have symbol as STK',async()=> {
         const symbol = await instance.symbol.call();
         assert.equal(symbol,'STK','Symbol is not STK');
     });
   });
-   
-  // Testing for transfers without approval
-  describe('transfers', ()=> {
-    it('Should transfer 50 eth from accounts[0] to accounts[1]', async()=> {
+
+  describe('Transfers without approvals', ()=> {
+    it('Should transfer 50 tokens from accounts[0] to accounts[1]', async()=> {
         await instance.transfer(recipientAccount, 50, {from: userAccount});
-        const balance = await instance.balanceOf.call(recipientAccount);
-        assert.strictEqual(balance.toNumber(), 50, 'recipient account did not have 50 eth extra');
+        const recipientBalance = await instance.balanceOf(recipientAccount);
+        assert.strictEqual(recipientBalance.toNumber(), 50, 'Oops the recipient account does not have the right amount');
     });
 
-    it("should fail transfering 2000000000 tokens from account[0] to account[1]", async () => {
-      return instance.transfer(recipientAccount, 1, { from: userAccount })
-        .then(() => instance.balanceOf(recipientAccount))
-        .catch((err) => assert(evmThrewError(err), err.message))
-    })
-/*
-    it("should fail transfering 2000000000 tokens from account[0] to account[1]", async () => {
-        let err = null;
-
-        try
-        {
-            await instance.transfer(recipientAccount, 500000000000000, { from: userAccount });
-            //assert.fail('This transaction should have failed');
-        }
-        catch(error)
-        {
-            err = error;
-        }
-
-        assert.ok(err instanceof Error);
+    it('should handle a transfer of zero', async () => {
+      assert(await instance.transfer(recipientAccount, 0, { from: userAccount }), 'transfer of zero has failed');
     });
-/*
-    it('Should fail when trying to transfer 1000000001 from accounts[0] to accounts[1]', async()=> {
-      expectThrow(await instance.transfer(recipientAccount, 10, {from: userAccount}));  
-  
-      try
-        {
-            await instance.transferFrom(userAccount, recipientAccount, 0);
-            assert.fail('This transaction should have failed');
-        }
-        catch(error)
-        {
-            assertRevert(error);
-        }
-    });*/
+
+    it('Should fail when 1000000001 tokens are transfered from accounts[0] to accounts[1]', async()=> {
+        await instance.transfer(recipientAccount, 1000000000000000001, { from: userAccount })
+        const recipientBalance = await instance.balanceOf(recipientAccount);
+        assert.strictEqual(recipientBalance.toNumber(), 50, 'the transaction did not fail like it was supposed to');
+    });
+  });
+
+  describe('Transfers with approvals', ()=> {
+      it('msg.sender should approve 50 to accounts[1]', async () => {
+        await instance.approve(recipientAccount, 50, { from: userAccount });
+        const recipientBalance = await instance.allowance(userAccount, recipientAccount);
+        assert.strictEqual(recipientBalance.toNumber(), 50);
+      });
   });
 
 });
