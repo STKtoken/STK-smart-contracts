0df4b25e6f083fb7e59c6c657e6679171d3560aa
aleksajovanovic <aleksa_jovanovic@hotmail.com>
2018-03-25 23:12:37 -0400

    conflict fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -41,7 +41,7 @@ module.exports = function(deployer, network, accounts)
           {
             return deployer.deploy(STKChannelLibrary).then(function()
               {
-              return deployer.deploy(STKToken,1000000000,'STK Token',18,'STK');
+              return deployer.deploy(STKToken,initialSupply,'STK Token',18,'STK');
                });
             });
         });
